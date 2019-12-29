30af2a5ef60c8dc3c2448d56dc19d217e186e22f
Akshay-Madrikar <akshaymadrikar@gmail.com>
2019-12-29 21:09:13 +0530

    added header component with site logo

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 
 import HomePage from './singlepages/homepage/homepage.component.jsx';
 import ShopPage from './singlepages/shop/shop.component.jsx';
+import Header from './components/header/header.component.jsx';
 
 
 function App () {
   return (
     <div>
+    <Header />
       <Switch>
         <Route exact={true} path='/' component={HomePage} />
         <Route exact={true} path='/shop' component={ShopPage} />
--- src/singlepages/shop/shop.component.jsx
+++ src/singlepages/shop/shop.component.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import SHOP_DATA from './shop.data';
 
 import CollectionPreview from '../../components/collection-preview/collection-preview.component.jsx';
-import CollectionItem from '../../components/collection-item/collection-item.component.jsx';
 
 class ShopPage extends React.Component {
     constructor( props ) {
