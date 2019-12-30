ba928663eed7281d2f26050a5e5276a892053130
Akshay-Madrikar <akshaymadrikar@gmail.com>
2019-12-30 19:36:32 +0530

    added sign-in-and-sign-up page

--- src/App.js
+++ src/App.js
@@ -5,15 +5,17 @@ import './App.css';
 import HomePage from './singlepages/homepage/homepage.component.jsx';
 import ShopPage from './singlepages/shop/shop.component.jsx';
 import Header from './components/header/header.component.jsx';
+import SignInSignUpPage from './singlepages/sign-in-sign-up/sign-in-sign-up.component.jsx';
 
 
 function App () {
   return (
     <div>
-    <Header />
+      <Header />
       <Switch>
         <Route exact={true} path='/' component={HomePage} />
         <Route exact={true} path='/shop' component={ShopPage} />
+        <Route exact={true} path='/signin' component={SignInSignUpPage} />
       </Switch>
     </div>
   );
---
+++ src/singlepages/sign-in-sign-up/sign-in-sign-up.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './sign-in-sign-up.styles.scss';
+
+const SignInSignUpPage = () => (
+    <div className='sign-in-and-sign-up'>
+        SIGN IN
+    </div>
+);
+
+export default SignInSignUpPage;
