2612eeb1d802b58adebc6f8752f9782f2f5dfe95
Akshay Madrikar <akshaymadrikar@gmail.com>
2020-02-28 20:59:44 +0530

    added cart components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,9 +3,13 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import { auth } from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropDown from '../cart-dropdown/cart-dropdown.component';
+
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.styles.scss';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 const Header = ( {currentUser} ) => (
 
@@ -26,7 +30,9 @@ const Header = ( {currentUser} ) => (
                     :
                     <Link className='option' to='/signin'>SIGN IN</Link>
             }
+            <CartIcon />
         </div>
+        <CartDropdown/>
     </div>
 )
 
