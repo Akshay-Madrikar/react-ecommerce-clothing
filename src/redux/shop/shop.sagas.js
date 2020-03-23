0a9b9e7d5c84137f0696dd2ec80a1004db9af63f
Akshay Madrikar <akshaymadrikar@gmail.com>
2020-03-24 01:16:49 +0530

    modifications

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,9 +1,27 @@
-import { takeEvery } from 'redux-saga/effects';
+import { takeEvery, call, put } from 'redux-saga/effects';
+
+import { firestore, convertCollectionSnapshotToMap } from '../../firebase/firebase.utils';
+import { fetchCollectionsSuccess, fetchCollectionsFailure } from './shop.actions';
 
 import ShopActionTypes from './shop.types';
 
 export function* fetchCollectionsAsync() {
-    yield console.log('I am fired!');
+
+    try {
+        const collectionRef = firestore.collection('collections');
+        const snapShot = yield collectionRef.get();
+        const collectionsMap = yield call( convertCollectionSnapshotToMap, snapShot );
+
+        yield put( fetchCollectionsSuccess( collectionsMap ) );
+    } catch( error ) {
+        yield put( fetchCollectionsFailure( error.message ) );
+    }
+
+    // ------ Thunk example ---------
+    //     collectionRef.get().then(snapShot => {
+    //         const collectionsMap = convertCollectionSnapshotToMap( snapShot );
+    //         dispatch( fetchCollectionsSuccess( collectionsMap ) );
+    // } ).catch( error => dispatch( fetchCollectionsFailure( error.message ) ) );
 }
 
 export function* fetchCollectionsStart() {
